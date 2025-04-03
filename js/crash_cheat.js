9711b8e0df2ab72d9e55a002b02bc2c15423f7c3
SEML04 <ds2718281828@outlook.com>
2025-04-03 12:56:45 +0800

    update myblog by Chihaya Anon

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,1 +1,16 @@
-var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/joke.ico"),document.title="😠很不高興為你服務😠",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="🥱很高興為你服務🥱",titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/joke.ico");
+        document.title = '😠很不高興為你服務😠';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '🥱很高興為你服務🥱';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
